02dbbd7a52018e48c4a0b76a659dac495d3cb79d
Elie <eliefenohasina@gmail.com>
2023-10-26 16:32:50 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+const video = document.getElementById('video')
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+    faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
+]).then(startVideo)
+
+function startVideo() {
+    navigator.getUserMedia(
+        { video: {} },
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    )
+}
+
+function getLabeledFaceDescriptions() {
+    const labels = ["Elie", "Autre"];
+    return Promise.all(
+        labels.map(async (label) => {
+            const descriptions = [];
+            for (let i = 1; i <= 2; i++) {
+                const img = await faceapi.fetchImage(`./labels/${label}/${i}.jpg`);
+                const detections = await faceapi
+                    .detectSingleFace(img)
+                    .withFaceLandmarks()
+                    .withFaceDescriptor();
+                descriptions.push(detections.descriptor);
+            }
+            return new faceapi.LabeledFaceDescriptors(label, descriptions);
+        })
+    );
+}
+
+video.addEventListener('play', async () => {
+
+    const labeledFaceDescriptors = await getLabeledFaceDescriptions();
+    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors);
+
+    let res = document.querySelector("#resultat")
+
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.querySelector(".faciale").prepend(canvas)
+    const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    setInterval(async () => {
+        // const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptors()
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+        // console.log(canvas);
+        // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+        // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+
+        const results = resizedDetections.map((im) => {
+            // console.log(im);
+            return faceMatcher.findBestMatch(im.descriptor);
+        });
+        results.forEach((result, i) => {
+
+            // console.log(result);
+            const box = resizedDetections[i].detection.box;
+            const drawBox = new faceapi.draw.DrawBox(box, {
+                label: result,
+            });
+            drawBox.draw(canvas);
+
+            res.innerHTML += result.label + " a éssayé d'entrer dans le bureau de vote n° 01, le " + new Date().toLocaleString() + "<br>----------------------<br>"
+            res.scrollIntoView(false);
+
+            if(result.label == "Elie"){
+                setInterval(()=>{
+                    location.href ="/election.html"
+                }, 5000)
+            }
+        });
+    }, 100)
+})
